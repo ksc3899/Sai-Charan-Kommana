33cc15fb6f709d4160da59258af9dd3e66ebe7d8
Sai Charan Kommana <saicharan.k3899@gmail.com>
2019-09-13 19:52:03 +0530

    Updates

---
+++ precache-manifest.38d6a088136077b5d3f494e5b9fc4300.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a194e1448dc535bb5a1ffeab73d41b9",
+    "url": "/saicharan/index.html"
+  },
+  {
+    "revision": "b379dc08ea1b408599ca",
+    "url": "/saicharan/static/css/main.98d09266.chunk.css"
+  },
+  {
+    "revision": "25be4c598a07f8dd5516",
+    "url": "/saicharan/static/js/2.7aa870dc.chunk.js"
+  },
+  {
+    "revision": "b379dc08ea1b408599ca",
+    "url": "/saicharan/static/js/main.92fab295.chunk.js"
+  },
+  {
+    "revision": "82d203879cd29ac7c65d",
+    "url": "/saicharan/static/js/runtime~main.8da39fa4.js"
+  },
+  {
+    "revision": "260c93b9cffb997af22a04bf66ca0f4d",
+    "url": "/saicharan/static/media/7-stones.260c93b9.png"
+  },
+  {
+    "revision": "ee66ba059b2f422762aa7f0ba4eeab3c",
+    "url": "/saicharan/static/media/down-arrow.ee66ba05.png"
+  },
+  {
+    "revision": "9e41deb3ddf04d6d4e4aa987e8c20e5e",
+    "url": "/saicharan/static/media/fruit-fight.9e41deb3.png"
+  },
+  {
+    "revision": "4d34922311fcc248e0b109fad120742c",
+    "url": "/saicharan/static/media/green-belt-certification.4d349223.png"
+  },
+  {
+    "revision": "4d0c761fe97885e97e379b41c506c689",
+    "url": "/saicharan/static/media/hero.4d0c761f.jpg"
+  },
+  {
+    "revision": "92a765cb9274018e7eb8c43ed56d0b5a",
+    "url": "/saicharan/static/media/space-fighter.92a765cb.png"
+  },
+  {
+    "revision": "00f30d3ee6f7cc73b39249d1c9a4d980",
+    "url": "/saicharan/static/media/survival.00f30d3e.png"
+  },
+  {
+    "revision": "53afa1c1fd2a32be487cf6d795544eaa",
+    "url": "/saicharan/static/media/tanks.53afa1c1.png"
+  },
+  {
+    "revision": "fc6a1536a2437553a6ad2120fcd8b2e0",
+    "url": "/saicharan/static/media/unity-certification.fc6a1536.png"
+  }
+]);
